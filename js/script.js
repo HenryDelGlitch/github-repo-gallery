6a0105171355bb54b83f245cd560cbe2294b6a0b
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-04-19 16:52:44 -0400

    Added a function to fetch my repo name using the GitHub API and fixed issues caused by having two username variables

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const profileOverview = document.querySelector(".overview");
 
 let username = "HenryDelGlitch";
 
+const repoList = document.querySelector(".repo-list");
+
 const gitInfo = async function(){
-    username = await fetch(`https://api.github.com/users/${username}`);
-    const rec = await username.json();
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const rec = await userInfo.json();
     gitUserData(rec);
 };
 
@@ -25,5 +27,20 @@ const gitUserData = async function(json){
         </div> `;
 
     profileOverview.append(div);
+    gitRepos();
 };
 
+const gitRepos = async function(){
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=40`);
+    const repoData = await repos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = async function(repos){
+    for(const projects of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${projects.name}</h3>`;
+        repoList.append(li);
+    }
+};
